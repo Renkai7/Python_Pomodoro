bd6353eea241be4c2738d4fe565aa47443e6c6a7
Mike <ms53150@gmail.com>
2022-04-29 16:41:16 -0400

    Add canvas image, start/reset button and labels

--- main.py
+++ main.py
@@ -17,3 +17,28 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# canvas & img
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=tomato_img)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 50, "bold"))
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+
+
+window.mainloop()
